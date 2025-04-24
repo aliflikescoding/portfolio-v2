74d7088b35c1c71cbc0bb4e94ea61c15be6e5589
Alif Wibowo <alif.likes.coding@gmail.com>
2025-04-24 08:41:22 +0700

    Add social link to about

--- src/app/[locale]/about/page.js
+++ src/app/[locale]/about/page.js
@@ -3,6 +3,10 @@ import { useTranslations, useLocale } from "next-intl";
 import { AnimatePresence } from "framer-motion";
 import FramerMotionWrapper from "@/components/FramerMotionWrapper";
 import Image from "next/image";
+import { IoLogoInstagram } from "react-icons/io";
+import { IoLogoLinkedin } from "react-icons/io";
+import { IoLogoGithub } from "react-icons/io";
+import Link from "next/link";
 
 const About = () => {
   const t = useTranslations("About");
@@ -18,9 +22,31 @@ const About = () => {
               sizes="100vw"
               className="w-auto h-auto max-w-[25%] mask mask-circle shadow-lg"
             />
-            <p className="font-light textarea-xl custom-shadow">
-              {t("paragraph")}
-            </p>
+            <div>
+              <p className="font-light text-2xl custom-shadow">
+                {t("paragraph")}
+              </p>
+              <div className="mt-4 flex gap-3">
+                <Link
+                  href={`https://www.instagram.com/alifwbwo/`}
+                  target="_blank"
+                >
+                  <IoLogoInstagram className="text-4xl text-primary" />
+                </Link>
+                <Link
+                  href={`https://www.linkedin.com/in/alifwibowo/`}
+                  target="_blank"
+                >
+                  <IoLogoLinkedin className="text-4xl text-primary" />
+                </Link>
+                <Link
+                  href={`https://github.com/aliflikescoding`}
+                  target="_blank"
+                >
+                  <IoLogoGithub className="text-4xl text-primary" />
+                </Link>
+              </div>
+            </div>
           </div>
         </FramerMotionWrapper>
       </div>
