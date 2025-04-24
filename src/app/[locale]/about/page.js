770b8595a9d13568ff7b5ce06a0381986223a5af
Alif Wibowo <alif.likes.coding@gmail.com>
2025-04-24 08:55:10 +0700

    Add home button to about

--- src/app/[locale]/about/page.js
+++ src/app/[locale]/about/page.js
@@ -6,24 +6,32 @@ import Image from "next/image";
 import { IoLogoInstagram } from "react-icons/io";
 import { IoLogoLinkedin } from "react-icons/io";
 import { IoLogoGithub } from "react-icons/io";
+import { FaHouse } from "react-icons/fa6";
 import Link from "next/link";
 
 const About = () => {
   const t = useTranslations("About");
   return (
     <AnimatePresence>
+      <Link
+        href={`/`}
+        className="text-2xl p-4 rounded-full cursor-pointer bg-gray-200 hover:bg-gray-300 transition absolute top-2 left-2"
+      >
+        <FaHouse className="text-primary"/>
+      </Link>
       <div className="h-screen flex flex-col items-center justify-center">
         <FramerMotionWrapper duration={1.5}>
-          <div className="max-w-[80%] mx-auto flex items-center justify-center gap-10">
+          <div className="xl:max-w-[80%] p-6 mx-auto flex flex-col md:flex-row items-center justify-center gap-10">
             <Image
               src={"/alif.png"}
               width={0}
               height={0}
               sizes="100vw"
-              className="w-auto h-auto max-w-[25%] mask mask-circle shadow-lg"
+              className="w-auto h-auto max-w-[80%] md:max-w-[25%] mask mask-circle shadow-lg"
+              alt="picture of alif"
             />
             <div>
-              <p className="font-light text-2xl custom-shadow">
+              <p className="font-light text-lg xl:text-2xl custom-shadow">
                 {t("paragraph")}
               </p>
               <div className="mt-4 flex gap-3">
